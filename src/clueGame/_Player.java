119dbfbc8d5c6bd4050916f44faaef85f5b5da01
cdeskin <cdeskin@mymail.mines.edu>
2012-10-28 11:50:57 -0600

    _Player done for now, may still need more functionality

--- src/clueGame/_Player.java
+++ src/clueGame/_Player.java
@@ -28,9 +28,13 @@ public abstract class _Player {
 	// Helpers for test purposes
 	public void giveCard(_Card card){
 		//Add card to players card array
+		if(!cards.contains(card))
+			cards.add(card);
 	}
 	public boolean hasCard(_Card card){
-		//return true if the card is in the players card array
-		return false;	
+		if(cards.contains(card))
+			return true;
+		else
+			return false;	
 	}
 }
