77dd35f5e463adb1dea92317085dd2433c1dc1b7
cdeskin <cdeskin@mymail.mines.edu>
2012-10-28 11:41:44 -0600

    adding changes

--- src/clueGame/_Player.java
+++ src/clueGame/_Player.java
@@ -1,6 +1,7 @@
 package clueGame;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 public abstract class _Player {
 	
@@ -8,6 +9,21 @@ public abstract class _Player {
 	public String name;
 	public Color color;
 	// then a variable to hold a list of cards.
+	ArrayList<_Card> cards;
+	
+	public _Player(){
+		super();
+		this.color = Color.black;
+		this.name = "Captain Cadaver";
+		this.cards = new ArrayList<_Card>();
+	}
+	
+	public _Player(String name, Color color, ArrayList<_Card> cards) {
+		super();
+		this.name = name;
+		this.color = color;
+		this.cards = cards;
+	}
 	
 	// Helpers for test purposes
 	public void giveCard(_Card card){
